e8d2d97f715092c4456d90a5bfb6092b7490a077
aentan <hello@aentan.com>
2014-01-28 16:19:42 +0800

    tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,7 @@
 $(function(){
-  
-  // Masthead wallpaper
-  $("#masthead").backstretch("assets/img/wallpaper.png");
-  $("#masthead-biz").backstretch("assets/img/wallpaper_biz2.jpg");
 
-  // Benefit backgrounds
-  $('.benefit').each(function() {
+  // Backgrounds
+  $('#masthead, .benefit').each(function() {
     $b = $(this);
     if ($b.data('background')) {
       $b.backstretch($b.data('background'));
