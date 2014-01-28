4f3300294f3db489f055d0510c8ecea8097c52fb
aentan <hello@aentan.com>
2014-01-28 16:02:39 +0800

    biz page coded

--- Build/assets/js/app.js
+++ Build/assets/js/app.js
@@ -2,6 +2,15 @@ $(function(){
   
   // Masthead wallpaper
   $("#masthead").backstretch("assets/img/wallpaper.png");
+  $("#masthead-biz").backstretch("assets/img/wallpaper_biz.jpg");
+
+  // Benefit backgrounds
+  $('.benefit').each(function() {
+    $b = $(this);
+    if ($b.data('background')) {
+      $b.backstretch($b.data('background'));
+    }
+  });
   
   $('.home-activity').imagefill();
 
